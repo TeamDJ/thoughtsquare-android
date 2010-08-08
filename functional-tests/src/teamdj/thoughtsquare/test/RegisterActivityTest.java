75a0913d6891135b3fde9255cbf3471f7b53d82a
duana <strongcoffee@gmail.com>
2010-08-08 20:51:05 +1000

    started RegisterActivity

---
+++ functional-tests/src/teamdj/thoughtsquare/test/RegisterActivityTest.java
@@ -0,0 +1,34 @@
+package thoughtsquare.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import teamdj.thoughtsquare.R;
+import teamdj.thoughtsquare.RegisterActivity;
+import teamdj.thoughtsquare.ThoughtSquareActivity;
+
+
+public class RegisterActivityTest extends ActivityInstrumentationTestCase2<RegisterActivity> {
+
+    public RegisterActivityTest() {
+        super("teamdj.thoughtsquare", RegisterActivity.class);
+    }
+
+    
+//    public void testRegistration(){
+//
+//        EditText displayNameField = (EditText) getActivity().findViewById(R.id.displayNameField);
+//        EditText usernameField = (EditText) getActivity().findViewById(R.id.usernameField);
+//        Button registerButton = (Button) getActivity().findViewById(R.id.registerButton);
+//
+//        displayNameField.setText("Julian Oliver");
+//        usernameField.setText("joliver");
+//        registerButton.performClick();
+//
+//    }
+
+
+
+}
+    
