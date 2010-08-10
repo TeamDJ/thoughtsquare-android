926f1d9aef8cf36ebd5ecb2a0959c6bd12511aac
duana <strongcoffee@gmail.com>
2010-08-11 02:17:43 +1000

    get functional test working with robotium

--- functional-tests/src/teamdj/thoughtsquare/test/ThoughtSquareActivityTest.java
+++ functional-tests/src/teamdj/thoughtsquare/test/ThoughtSquareActivityTest.java
@@ -2,23 +2,45 @@ package teamdj.thoughtsquare.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.TextView;
+//import com.jayway.android.robotium.solo.Solo;
+import com.jayway.android.robotium.solo.Solo;
 import teamdj.thoughtsquare.R;
 import teamdj.thoughtsquare.ThoughtSquareActivity;
 
 
 public class ThoughtSquareActivityTest extends ActivityInstrumentationTestCase2<ThoughtSquareActivity> {
 
+    private Solo solo;
+
+
     public ThoughtSquareActivityTest() {
         super("teamdj.thoughtsquare", ThoughtSquareActivity.class);
     }
 
-    
-    public void testWelcomeTextIsPresent(){
-        TextView textView = (TextView) getActivity().findViewById(R.id.welcome_label);
-        assertEquals("Hello World, ThoughtSquareActivity", textView.getText());
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
     }
 
 
+    public void testWelcomeTextIsPresent() {
+        solo.assertCurrentActivity("Expected ThoughtSquareActivity activity", "ThoughtSquareActivity");
 
+
+        TextView textView = (TextView) getActivity().findViewById(R.id.welcome_label);
+        assertEquals("Hello World, ThoughtSquareActivity", textView.getText().toString());
+    }
+
+
+    @Override
+    public void tearDown() throws Exception {
+        try {
+            solo.finalize();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        getActivity().finish();
+        super.tearDown();
+    }
 }
     
