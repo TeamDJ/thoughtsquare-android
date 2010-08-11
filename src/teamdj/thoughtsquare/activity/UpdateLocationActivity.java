cb2121555f4d36fa1547517a5787b00922504dd5
James Ottaway <james@ottaway.mp>
2010-08-11 16:58:21 +1000

    Added static list items to UpdateLocationActivity

--- src/teamdj/thoughtsquare/activity/UpdateLocationActivity.java
+++ src/teamdj/thoughtsquare/activity/UpdateLocationActivity.java
@@ -1,8 +1,15 @@
 package teamdj.thoughtsquare.activity;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import teamdj.thoughtsquare.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,10 +18,14 @@ import teamdj.thoughtsquare.R;
  * Time: 4:27:12 PM
  * To change this template use File | Settings | File Templates.
  */
-public class UpdateLocationActivity extends Activity {
+public class UpdateLocationActivity extends ListActivity {
+    private List<String> cities = asList("Brisbane", "Sydney", "Melbourne", "Perth");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);    //To change body of overridden methods use File | Settings | File Templates.
         setContentView(R.layout.update_location);
+        
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cities));
     }
 }
