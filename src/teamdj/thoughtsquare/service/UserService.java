cbd19035437c4c32881e284b935c2f9471922f8d
James Ottaway <james@ottaway.mp>
2010-08-12 08:38:31 +1000

    Renamed RegisterService to UserService

--- src/teamdj/thoughtsquare/service/UserService.java
+++ src/teamdj/thoughtsquare/service/UserService.java
@@ -5,24 +5,15 @@ import teamdj.thoughtsquare.utility.AHTTPClient;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by IntelliJ IDEA.
- * User: jottaway
- * Date: Aug 11, 2010
- * Time: 1:45:32 PM
- * To change this template use File | Settings | File Templates.
- */
-public class RegisterService {
+public class UserService {
     private AHTTPClient client;
 
-    public RegisterService(AHTTPClient client) {
+    public UserService(AHTTPClient client) {
         //To change body of created methods use File | Settings | File Templates.
         this.client = client;
     }
 
-
     public boolean register(String emailAddress, String displayName) {
-
         Map<String, String> postParams = new HashMap<String, String>();
         postParams.put("user[display_name]", displayName);
         postParams.put("user[email]", emailAddress);
@@ -30,6 +21,5 @@ public class RegisterService {
         int status = client.post("http://thoughtsquare.heroku.com/users.json", postParams);
 
         return status == 201;
-
     }
 }
