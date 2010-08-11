14f54eeabd842495121fa4d775f1b4f8b2fc08d8
James Ottaway <james@ottaway.mp>
2010-08-12 09:03:34 +1000

    Wired up MockLocationService to UpdateLocationActivity through new ArrayAdapter<Location>

--- src/teamdj/thoughtsquare/activity/UpdateLocationActivity.java
+++ src/teamdj/thoughtsquare/activity/UpdateLocationActivity.java
@@ -3,10 +3,16 @@ package teamdj.thoughtsquare.activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import teamdj.thoughtsquare.R;
+import teamdj.thoughtsquare.domain.Location;
+import teamdj.thoughtsquare.service.LocationService;
+import teamdj.thoughtsquare.service.MockLocationService;
 
 import java.util.List;
 
@@ -20,24 +26,48 @@ import static java.util.Arrays.asList;
  * To change this template use File | Settings | File Templates.
  */
 public class UpdateLocationActivity extends ListActivity {
-    private List<String> cities = asList("Brisbane", "Sydney", "Melbourne", "Perth");
+    private LocationService locationService;
+    private List<Location> locations;
+
+    public UpdateLocationActivity() {
+        locationService = new MockLocationService();
+        
+        locations = locationService.getLocations();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);    //To change body of overridden methods use File | Settings | File Templates.
         setContentView(R.layout.update_location);
 
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cities));
+        setListAdapter(new LocationAdapter());
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         Bundle extras = new Bundle();
-        extras.putString("cityName", cities.get(position));
+        extras.putInt("location_id", locations.get(position).getId());
 
         Intent mIntent = new Intent();
         mIntent.putExtras(extras);
         setResult(RESULT_OK, mIntent);
         finish();
+    }
+
+    class LocationAdapter extends ArrayAdapter<Location> {
+        LocationAdapter() {
+            super(UpdateLocationActivity.this, android.R.layout.simple_list_item_1, locations);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+
+            TextView text = (TextView)row.findViewById(android.R.id.text1);
+            text.setText(locations.get(position).getTitle());
+
+            return row;
+        }
     }
 }
