e51f5578c6b4b7d4a9b6f758b06ba67e4e454ea1
duana <strongcoffee@gmail.com>
2010-08-15 16:09:35 +1000

    wrap json object to get rid of stupid JSONExceptions

--- src/teamdj/thoughtsquare/builder/UserBuilder.java
+++ src/teamdj/thoughtsquare/builder/UserBuilder.java
@@ -1,19 +1,14 @@
 package teamdj.thoughtsquare.builder;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import teamdj.thoughtsquare.domain.User;
+import teamdj.thoughtsquare.utility.JSONObject;
 
 public class UserBuilder {
     public User fromJSON(String input) {
-        try {
             JSONObject jsonUser = new JSONObject(input).getJSONObject("user");
             int id = jsonUser.getInt("id");
             String email = jsonUser.getString("email");
             String displayName = jsonUser.getString("display_name");
             return new User(id, email, displayName);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
