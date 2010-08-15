2b1f31f2637bc2844fd796dbc69ac99e891ef71b
duana <strongcoffee@gmail.com>
2010-08-15 22:58:59 +1000

    ok now build is working properly (hopefully)

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 ruby fake-server.rb &
 # kill fake-server on interrupts or when process exits
-trap "cleanup()" 0 2 9 15
+trap "cleanup" 0 2 9 15
 
 cp env/local/application.properties assets/application.properties
 ant clean uninstall 
-cd functional-tests
-ant clean run-tests
+# do the following in a subshell so this script stays in root dir
+(cd functional-tests; ant clean run-tests)
 
 cleanup(){
     kill -9 $(jobs -p)
