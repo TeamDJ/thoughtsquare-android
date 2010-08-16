d0fc13e0eece5a6de63d3c1210c79fe055470d6c
James Ottaway <james@ottaway.mp>
2010-08-17 09:31:51 +1000

    User.updateLocation() is now wired up to the backend

--- src/teamdj/thoughtsquare/domain/User.java
+++ src/teamdj/thoughtsquare/domain/User.java
@@ -8,6 +8,9 @@ import teamdj.thoughtsquare.utility.Config;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.String.valueOf;
+import static org.apache.http.HttpStatus.SC_OK;
+
 public class User {
     private int id;
     private UserProvider userProvider;
@@ -15,6 +18,7 @@ public class User {
     private Config config;
     private String email;
     private String displayName;
+    private Location currentLocation;
 
     public User(UserProvider userProvider, AHTTPClient client, Config config, String email, String displayName) {
         this.userProvider = userProvider;
@@ -36,6 +40,9 @@ public class User {
         return displayName;
     }
 
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
 
     public boolean register() {
         Map<String, String> postParams = new HashMap<String, String>();
@@ -49,12 +56,24 @@ public class User {
             userProvider.saveUser(this);
             return true;
         }
-        else{
-            return false;
-        }
+
+        return false;
+
     }
 
-    public void updateLocation(){
-        // call client to update location here.
+    public boolean updateLocation(Location newLocation) {
+        Map<String, String> putParams = new HashMap<String, String>();
+        putParams.put("user[display_name]", displayName);
+        putParams.put("user[email]", email);
+        putParams.put("user[current_location_id]", valueOf(newLocation.getId()));
+
+        AHTTPResponse status = client.put(config.getServerBaseURL() + "/users/" + id + ".json", putParams);
+
+        if (status.getResponseStatus() == SC_OK) {
+            this.currentLocation = newLocation;
+            return true;
+        }
+
+        return false;
     }
 }
