5f44e05fec770a01a5b98bef5351ebb26bbd46a1
James Ottaway <james@ottaway.mp>
2010-08-17 09:04:56 +1000

    Added put() method to AHTTPClient

--- integration-test/teamdj/thoughtsquare/utility/AHTTPClientIntegrationTest.java
+++ integration-test/teamdj/thoughtsquare/utility/AHTTPClientIntegrationTest.java
@@ -1,6 +1,7 @@
 package teamdj.thoughtsquare.utility;
 
 import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -9,7 +10,6 @@ import java.util.Map;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.isNull;
 
 /**
@@ -20,10 +20,15 @@ import static org.mockito.Matchers.isNull;
  * To change this template use File | Settings | File Templates.
  */
 public class AHTTPClientIntegrationTest {
-    @Test
-    public void testPost() throws Exception {
-        AHTTPClient ahttpClient = new AHTTPClient();
+    private AHTTPClient ahttpClient;
 
+    @Before
+    public void setUp() {
+        ahttpClient = new AHTTPClient();
+    }
+
+    @Test
+    public void shouldPostSuccessfully() throws Exception {
         Map<String, String> postParams = new HashMap<String, String>();
         postParams.put("user[display_name]", "foo");
         postParams.put("user[email]", "foo@bar.com");
@@ -32,6 +37,18 @@ public class AHTTPClientIntegrationTest {
 
         assertThat(response.getResponseStatus(), is(HttpStatus.SC_CREATED));
         assertThat(response.getResponseBody(), not(isNull()));
+    }
+
+    @Test
+    public void shouldPutSuccessfully() {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("user[display_name]", "James");
+        params.put("user[email]", "foo@bar.com");
+        params.put("user[current_location_id]", "2");
+
+        AHTTPResponse response = ahttpClient.put("http://thoughtsquare.heroku.com/users/1.json", params);
+
+        assertThat(response.getResponseStatus(), is(HttpStatus.SC_OK));
     }
 
     //TODO: Clean up created user after test.
--- src/teamdj/thoughtsquare/utility/AHTTPClient.java
+++ src/teamdj/thoughtsquare/utility/AHTTPClient.java
@@ -6,6 +6,8 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
@@ -16,8 +18,13 @@ import java.util.Map;
 
 
 public class AHTTPClient {
+    private HttpClient httpClient;
+
+    public AHTTPClient() {
+        this.httpClient = new DefaultHttpClient();
+    }
+
     public AHTTPResponse post(String url, Map<String, String> postParams) {
-        HttpClient httpClient = new DefaultHttpClient();
         HttpPost post = new HttpPost(url);
 
         List<NameValuePair> params = getParams(postParams);
@@ -37,7 +44,27 @@ public class AHTTPClient {
         return getResponse(httpClient, post, response);
     }
 
-    private AHTTPResponse getResponse(HttpClient httpClient, HttpPost post, HttpResponse response) {
+    public AHTTPResponse put(String url, Map<String, String> putParams) {
+        HttpPut put = new HttpPut(url);
+
+        List<NameValuePair> params = getParams(putParams);
+        try {
+            put.setEntity(new UrlEncodedFormEntity(params));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        HttpResponse response;
+        try {
+            response = httpClient.execute(put);
+        } catch (IOException e) {
+            throw new RuntimeException("Error POSTing", e);
+        }
+
+        return getResponse(httpClient, put, response);
+    }
+
+    private AHTTPResponse getResponse(HttpClient httpClient, HttpRequestBase post, HttpResponse response) {
         String responseBody = null;
         int responseStatus = response.getStatusLine().getStatusCode();
 
