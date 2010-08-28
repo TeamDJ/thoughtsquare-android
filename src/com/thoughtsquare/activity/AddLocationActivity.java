1237f1670238b8e2459458d2a6c49f1f29158e8b
James Ottaway <james@ottaway.mp>
2010-08-28 10:23:38 +1000

    Specced out basic Add Location view

--- src/com/thoughtsquare/activity/AddLocationActivity.java
+++ src/com/thoughtsquare/activity/AddLocationActivity.java
@@ -1,6 +1,13 @@
 package com.thoughtsquare.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import com.thoughtsquare.R;
 
 public class AddLocationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_location);
+    }
 }
