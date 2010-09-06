395affc1b58faf6f47e505e6f38650f55c277109
James Ottaway <james@ottaway.mp>
2010-09-06 22:42:08 +1000

    added JSONArray utility class which is used by AHTTPResponse to offer another convenience JSON method

--- src/com/thoughtsquare/utility/JSONArray.java
+++ src/com/thoughtsquare/utility/JSONArray.java
@@ -5,7 +5,6 @@ import org.json.JSONException;
 public class JSONArray {
     private org.json.JSONArray jsonArray;
 
-
     public JSONArray(String s){
         if(s==null)throw new IllegalArgumentException("s cannot be null");
         try {
@@ -13,6 +12,10 @@ public class JSONArray {
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public JSONArray(org.json.JSONArray jsonArray) {
+        this.jsonArray = jsonArray;
     }
 
     public JSONObject getJSONObject(int i){
@@ -23,10 +26,15 @@ public class JSONArray {
         }
     }
 
+    public Object get(int index) {
+        try {
+            return jsonArray.get(index);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public int length(){
         return jsonArray.length();
     }
-
-
-    
 }
